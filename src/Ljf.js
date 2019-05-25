fd870b8a0e98ca252d5e35426ac0fab1f173cb40
adrianAlday <adrian.alday@leafly.com>
2019-05-25 13:41:09 -0400

    fix time, add date

--- src/Ljf.js
+++ src/Ljf.js
@@ -7,20 +7,23 @@ export default class Ljf extends Component {
     fans: [],
   }
 
-  componentDidMount() {
-    setInterval(() =>
-      client.getEmojis()
-      .then((response) => {
-        this.setState({
-          parents: response.parents
-            .filter(message => message.body.length > 0)
-            .sort((a, b) => a.time < b.time ? -1 : 1),
-          fans: response.fans
-            .filter(message => message.body.length > 0)
-            .sort((a, b) => a.time < b.time ? -1 : 1),
-        })
+  getData = () => {
+    client.getEmojis()
+    .then((response) => {
+      this.setState({
+        parents: response.parents
+          .filter(message => message.body.length > 0)
+          .sort((a, b) => a.time < b.time ? -1 : 1),
+        fans: response.fans
+          .filter(message => message.body.length > 0)
+          .sort((a, b) => a.time < b.time ? -1 : 1),
       })
-    , 3000);
+    })
+  }
+
+  componentDidMount() {
+    this.getData();
+    setInterval(() => this.getData(), 3000);
   }
 
   render() {
@@ -35,29 +38,35 @@ export default class Ljf extends Component {
         </div>
 
         <div className="ljf top padded">
-          {this.state.parents.map((message, i) =>
-            <div key={i}>
+          {this.state.parents.map((message, i) => {
+            let ruby_time = message.time
+            let military_hour = ruby_time.slice(11,13)
+            let hour = military_hour === 0
+                    ? 12
+                    : military_hour > 12
+                      ? military_hour - 12
+                      : military_hour
+            let min = ruby_time.slice(14,16) < 10
+                    ? '0' + ruby_time.slice(14,16)
+                    : ruby_time.slice(14,16)
+            let half = military_hour > 11 ? 'p' : 'a'
+            let full_month = ruby_time.slice(5,7)
+            let month = full_month.charAt(0) === '0'
+              ? full_month.slice(1)
+              : full_month
+           let full_date = ruby_time.slice(8,10)
+            let date = full_date.charAt(0) === '0'
+              ? full_date.slice(1)
+              : full_date
+            return <div key={i}>
               <span className="ljf big">
                 {message.body}
-                {/* {message.body.replace(/[A-Za-z0-9]/g, '')} */}
               </span>
               <span className="ljf small right">
-                {
-                  new Date(message.time).getHours() === 0
-                    ? 12
-                    : new Date(message.time).getHours() > 12
-                      ? new Date(message.time).getHours() - 12
-                      : new Date(message.time).getHours()
-                }:{
-                  new Date(message.time).getMinutes() < 10
-                    ? '0' + new Date(message.time).getMinutes()
-                    : new Date(message.time).getMinutes()
-                }{
-                  new Date(message.time).getHours() > 11 ? 'p' : 'a'
-                }
+                {hour}:{min}{half} {month}/{date}
               </span>
             </div>
-          )}
+          })}
         </div>
 
         <div className="ljf small padded">
@@ -65,31 +74,35 @@ export default class Ljf extends Component {
         </div>
 
         <div className="ljf bottom padded">
-          {this.state.fans.map((message, i) =>
-            <div key={i}>
+          {this.state.fans.map((message, i) => {
+            let ruby_time = message.time
+            let military_hour = ruby_time.slice(11,13)
+            let hour = military_hour === 0
+                    ? 12
+                    : military_hour > 12
+                      ? military_hour - 12
+                      : military_hour
+            let min = ruby_time.slice(14,16) < 10
+                    ? '0' + ruby_time.slice(14,16)
+                    : ruby_time.slice(14,16)
+            let half = military_hour > 11 ? 'p' : 'a'
+            let full_month = ruby_time.slice(5,7)
+            let month = full_month.charAt(0) === '0'
+              ? full_month.slice(1)
+              : full_month
+           let full_date = ruby_time.slice(8,10)
+            let date = full_date.charAt(0) === '0'
+              ? full_date.slice(1)
+              : full_date
+            return <div key={i}>
               <span className="ljf medium">
-                {/* {message.body} */}
-                {message.body.replace(/[A-Za-z0-9]/g, '')}
+                {message.body}
               </span>
               <span className="ljf small right">
-                {
-                  new Date(message.time).getHours() === 0
-                    ? 12
-                    : new Date(message.time).getHours() > 12
-                      ? new Date(message.time).getHours() - 12
-                      : new Date(message.time).getHours()
-                }:{
-                  new Date(message.time).getMinutes() < 10
-                    ? '0' + new Date(message.time).getMinutes()
-                    : new Date(message.time).getMinutes()
-                }{
-                  new Date(message.time).getHours() > 11 ? 'p' : 'a'
-                }
-                &nbsp;☎-{message.from.substring(message.from.length - 4, message.from.length)}
-
+                {hour}:{min}{half} {month}/{date}
               </span>
             </div>
-          )}
+          })}
         </div>
 
         <div className="ljf small padded">
